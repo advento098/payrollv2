53331ca3060f3f30821c3243018a69104e6764d3
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-08-19 19:39:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Payroll version 2
+
+**_This will be built on top of react + tailwind, node, and postgresql_**
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="mx-100 bg-amber-50 p-0.5 top-0 text-3xl"></div>;
+}
+
+export default App;
