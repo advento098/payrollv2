0ecd610b32d28c333b6823acc4edc7a81cc4b137
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-08-23 15:55:11 +0800

    started the scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import centerTextLogo from '/bongkim_logo_center_text.svg';
+
 function App() {
-  return <>
-    
-  </>;
+  return (
+    <>
+      <header className="fixed top-0 left-0 z-100 w-full">
+        <img
+          src={centerTextLogo}
+          alt="bongkim_center_text"
+          className="mx-auto w-1/8 border-2"
+        />
+      </header>
+      <main className="h-dvh">
+        <nav className="h-full w-2xs border-2 border-red-700">
+          <ul className="list-none">
+            <li>a</li>
+          </ul>
+        </nav>
+      </main>
+    </>
+  );
 }
 
 export default App;
