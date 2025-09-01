131e79dba9963110607420a78e9a97767c6df974
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-09-01 22:36:07 +0800

    Placed the overflow to the main container

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -3,10 +3,12 @@ import Card from '../../../ui/Card';
 
 export default function Dashboard() {
   return (
-    <main className="h-dvh w-[80%] p-10 pb-0">
+    <main className="flex h-dvh w-[80%] flex-col overflow-y-scroll p-10 pb-0">
       {/* Cards holder */}
-      <div className="main-container flex h-full flex-col items-center gap-5 overflow-y-scroll border-2">
-        <div className="sticky top-0 z-10">---search bar here---</div>
+      <div className="sticky top-0 z-10 mx-auto mb-10">
+        ---search bar here---
+      </div>
+      <div className="main-container flex flex-col items-center gap-5">
         <Card type="simple"></Card>
         <Card type="simple"></Card>
         <Card type="simple"></Card>
