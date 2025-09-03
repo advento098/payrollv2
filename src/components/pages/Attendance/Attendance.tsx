a4f10c1a2cd5fd7111ac43fdde69938d1baf1e32
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-09-03 16:39:54 +0800

    cleaned unused declaration in attendance sheet

--- src/components/pages/Attendance/Attendance.tsx
+++ src/components/pages/Attendance/Attendance.tsx
@@ -1,22 +1,6 @@
-import clsx from 'clsx';
-import { useState } from 'react';
 import AttendanceSheet from './AttendanceSheet';
 
 export default function Attendance() {
-  const title: string = 'Angeles';
-  const [test, setTest] = useState<boolean>(false);
-  const [selector, setSelector] = useState<boolean>(false);
-
-  const handleSelectorClick = () => {
-    setSelector((prev) => !prev);
-  };
-
-  const variants = {
-    tableHeader: 'border-2 border-r-0 border-b-0 px-2 py-1 text-center',
-    tableData: 'border-2 border-r-0 min-w-40 border-b-0 px-2 py-1 text-center',
-    nullDataHeader: 'border-2 border-r-0 min-w-40 border-b-0 px-2 py-1',
-  } as const;
-
   return (
     <main className="flex h-dvh w-[80%] flex-col items-center overflow-y-scroll border-2">
       <AttendanceSheet />
