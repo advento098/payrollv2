f11fc843cac617a51f31822e9afc8a7b62e4393e
Pons Anthony Advento <ponsadvento08@gmail.com>
2025-09-24 21:55:05 +0800

    Added profile controller and model

---
+++ backend/payrollBackend/Models/Profile.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata;
+
+namespace payrollBackend;
+
+public class Profile
+{
+    public int Id { get; set; }
+    public string? Position { get; set; }
+    public string? SSS { get; set; }
+    public string? Philhealth { get; set; }
+    public string? PagIbig { get; set; }
+
+}
