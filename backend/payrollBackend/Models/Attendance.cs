81276d3fc6e08d248556d25a66d7624a810c7eba
Pons Anthony Advento <ponsadvento08@gmail.com>
2025-09-26 20:51:19 +0800

    Added attendance model and entity class

---
+++ backend/payrollBackend/Models/Attendance.cs
@@ -0,0 +1,12 @@
+﻿namespace payrollBackend;
+
+public class Attendance
+{
+    public int Id { get; set; }
+    /// <summary>
+    /// Either "Morning" or "Night"
+    /// </summary>
+    public string? Duty { get; set; }
+    public int? AttendanceLength { get; set; }
+    public int[]? AttendanceSheet { get; set; }
+}
---
+++ backend/payrollBackend/data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace payrollBackend;
+
+public class AppDbContext : DbContext
+{
+    // Constructor
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    
+
+}
