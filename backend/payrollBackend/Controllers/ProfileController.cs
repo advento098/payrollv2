3c8f50aa6421b6f3ffa3e0c9d5b906a5fe530d14
Pons Anthony Advento <ponsadvento08@gmail.com>
2025-10-02 20:56:44 +0800

    Added attendance and salary controller

--- backend/payrollBackend/Controllers/ProfileController.cs
+++ backend/payrollBackend/Controllers/ProfileController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyApp.Namespace
+namespace payrollBackend.Namespace
 {
     [Route("api/[controller]")]
     [ApiController]
