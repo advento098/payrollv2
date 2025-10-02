8b482f70e296d1a9b97cf86a3b82125c280eeb08
Pons Anthony Advento <ponsadvento08@gmail.com>
2025-10-02 20:57:00 +0800

    Added attendance and salary controller

---
+++ backend/payrollBackend/Controllers/AttendanceController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace payrollBackend.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+    }
+}
